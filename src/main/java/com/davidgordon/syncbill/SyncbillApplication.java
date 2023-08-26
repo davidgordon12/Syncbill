2b097b809d62bbd41058a9681b93497ee24e6ccf
tehtko <moderndsnsam@gmail.com>
2023-08-26 17:05:04 -0400

    Upload files

---
+++ src/main/java/com/davidgordon/syncbill/SyncbillApplication.java
@@ -0,0 +1,13 @@
+package com.davidgordon.syncbill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyncbillApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SyncbillApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/davidgordon/syncbill/SyncbillApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davidgordon.syncbill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyncbillApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
