9f53b467fa64c95d145d63313c9a2dd7b6409da8
tehtko <moderndsnsam@gmail.com>
2023-08-30 16:12:37 -0400

    Add crud methods and endpoints

--- api/src/main/java/com/davidgordon/syncbill/Invoice/InvoiceService.java
+++ api/src/main/java/com/davidgordon/syncbill/Invoice/InvoiceService.java
@@ -1,27 +1,62 @@
 package com.davidgordon.syncbill.Invoice;
 
 import java.time.LocalDate;
+import java.util.Optional;
 import java.util.UUID;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 @Service
 public class InvoiceService {
-  public Invoice createFakeInvoice() {
-    Invoice invoice = new Invoice();
-    
-    invoice.setId(UUID.randomUUID());
-    invoice.setFrom("TD Bank");
-    invoice.setTitle("Mortgage");
-    invoice.setCategory(InvoiceCategory.MORTGAGE);
-    invoice.setAmountDue(1727.27f);
-    invoice.setDateRecieved(LocalDate.now().minusDays(3));
-    invoice.setDueDate(LocalDate.now().plusDays(15));
+  private final InvoiceRepository invoiceRepository;
 
-    return invoice;
+  @Autowired
+  public InvoiceService(InvoiceRepository invoiceRepository) {
+    this.invoiceRepository = invoiceRepository;
   }
 
-  public Invoice createInvoice(Invoice invoice) {
-    return invoice;
+  public Invoice findInvoiceById(UUID id) {
+    Optional<Invoice> invoice = invoiceRepository.findById(id);
+
+    if(invoice.isPresent()) {
+      return invoice.get();
+    }
+    else {
+      return new Invoice();
+    }
+  }
+
+  public Invoice createInvoice(String invoiceJSON) {
+    try {
+      Gson g = new GsonBuilder()
+        .registerTypeAdapter(LocalDate.class, new InvoiceTypeAdapter().nullSafe())
+        .create();
+
+      Invoice invoice = g.fromJson(invoiceJSON, Invoice.class);
+      invoiceRepository.save(invoice);
+      return invoice;
+    }
+    catch(Exception e) {
+      System.out.println("There was a problem saving the invoice. Exception: " + e);
+      return null;
+    }
+  }
+
+  public Invoice deletInvoiceById(UUID id) {
+    try {
+      Optional<Invoice> invoice = invoiceRepository.findById(id);
+      if(invoice.isPresent()) {
+        invoiceRepository.delete(invoice.get());
+      }
+      return invoice.get();
+    } 
+    catch(Exception e) {
+      System.out.println("There was a problem deleting the invoice. Exception: " + e);
+      return null;
+    }
   }
 }
