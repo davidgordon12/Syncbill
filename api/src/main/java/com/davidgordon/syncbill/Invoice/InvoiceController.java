b0cf2e3992c85c142529804e8a031976ddd1bbc1
tehtko <moderndsnsam@gmail.com>
2023-08-30 00:22:11 -0400

    Create Get and Post mappings for invoices

--- api/src/main/java/com/davidgordon/syncbill/Invoice/InvoiceController.java
+++ api/src/main/java/com/davidgordon/syncbill/Invoice/InvoiceController.java
@@ -1,12 +1,18 @@
 package com.davidgordon.syncbill.Invoice;
 
+import java.time.LocalDate;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 @RestController
 @RequestMapping(path = "api/v1/invoice")
@@ -20,12 +26,27 @@ public class InvoiceController {
 
   @GetMapping("/getInvoice/{id}")
   public Invoice getInvoiceById(@PathVariable UUID id) {
-    Invoice invoice = invoiceService.getInvoiceById(id);
+    Invoice invoice = invoiceService.createFakeInvoice();
 
     if(invoice != null) {
       return invoice;
     }
 
-    return new Invoice();
+    return invoice;
+  }
+
+  @PostMapping("/createInvoice") 
+  public Invoice createInvoice(@RequestBody String invoiceJSON) {
+    Gson g = new GsonBuilder()
+      .registerTypeAdapter(LocalDate.class, new InvoiceTypeAdapter().nullSafe())
+      .create();
+
+    Invoice invoice = g.fromJson(invoiceJSON, Invoice.class);
+
+    if(invoiceService.createInvoice(invoice) != null) {
+      return invoice;
+    }
+
+    throw new IllegalArgumentException("The invoice was in an incorrect format");
   }
 }
