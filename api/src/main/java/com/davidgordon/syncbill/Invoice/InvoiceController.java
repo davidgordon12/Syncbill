303659ffdc204a3f0601d95be391eb13025d36f8
tehtko <moderndsnsam@gmail.com>
2023-08-29 20:20:24 -0400

    Implement getInvoiceById

--- api/src/main/java/com/davidgordon/syncbill/Invoice/InvoiceController.java
+++ api/src/main/java/com/davidgordon/syncbill/Invoice/InvoiceController.java
@@ -1,6 +1,10 @@
 package com.davidgordon.syncbill.Invoice;
 
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,5 +18,14 @@ public class InvoiceController {
     this.invoiceService = invoiceService;
   }
 
-  
+  @GetMapping("/getInvoice/{id}")
+  public Invoice getInvoiceById(@PathVariable UUID id) {
+    Invoice invoice = invoiceService.getInvoiceById(id);
+
+    if(invoice != null) {
+      return invoice;
+    }
+
+    return new Invoice();
+  }
 }
--- api/src/main/java/com/davidgordon/syncbill/Invoice/InvoiceService.java
+++ api/src/main/java/com/davidgordon/syncbill/Invoice/InvoiceService.java
@@ -1,13 +1,25 @@
 package com.davidgordon.syncbill.Invoice;
 
+import java.util.Optional;
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class InvoiceService {
-  
-  @Autowired
-  public InvoiceService() {
+  private final InvoiceRepository invoiceRepository;
 
+  @Autowired
+  public InvoiceService(InvoiceRepository invoiceRepository) {
+    this.invoiceRepository = invoiceRepository;
   }
 
+  public Invoice getInvoiceById(UUID id) {
+    Optional<Invoice> invoice = invoiceRepository.getInvoiceById(id);
 
+    if(invoice.isPresent()) {
+      return invoice.get();
+    }
+    
+    return null;
+  }
 }
