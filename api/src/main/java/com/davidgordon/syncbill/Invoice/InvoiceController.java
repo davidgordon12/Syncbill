26f7d8fca3394a43d34285fab019c868d0e2401a
tehtko <moderndsnsam@gmail.com>
2023-08-29 20:06:21 -0400

    Create InvoiceController

--- api/src/main/java/com/davidgordon/syncbill/Invoice/InvoiceController.java
+++ api/src/main/java/com/davidgordon/syncbill/Invoice/InvoiceController.java
@@ -1,5 +1,18 @@
 package com.davidgordon.syncbill.Invoice;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "api/v1/invoice")
 public class InvoiceController {
+  private final InvoiceService invoiceService;
+
+  @Autowired
+  public InvoiceController(InvoiceService invoiceService) {
+    this.invoiceService = invoiceService;
+  }
+
   
 }
