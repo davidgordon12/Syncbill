cb63468871ae00936f82dac114e43a0ed97af892
tehtko <moderndsnsam@gmail.com>
2023-08-30 00:23:02 -0400

    Create debugging methods

--- api/src/main/java/com/davidgordon/syncbill/Invoice/InvoiceService.java
+++ api/src/main/java/com/davidgordon/syncbill/Invoice/InvoiceService.java
@@ -1,25 +1,27 @@
 package com.davidgordon.syncbill.Invoice;
 
-import java.util.Optional;
+import java.time.LocalDate;
 import java.util.UUID;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class InvoiceService {
-  private final InvoiceRepository invoiceRepository;
+  public Invoice createFakeInvoice() {
+    Invoice invoice = new Invoice();
+    
+    invoice.setId(UUID.randomUUID());
+    invoice.setFrom("TD Bank");
+    invoice.setTitle("Mortgage");
+    invoice.setCategory(InvoiceCategory.MORTGAGE);
+    invoice.setAmountDue(1727.27f);
+    invoice.setDateRecieved(LocalDate.now().minusDays(3));
+    invoice.setDueDate(LocalDate.now().plusDays(15));
 
-  @Autowired
-  public InvoiceService(InvoiceRepository invoiceRepository) {
-    this.invoiceRepository = invoiceRepository;
+    return invoice;
   }
 
-  public Invoice getInvoiceById(UUID id) {
-    Optional<Invoice> invoice = invoiceRepository.getInvoiceById(id);
-
-    if(invoice.isPresent()) {
-      return invoice.get();
-    }
-    
-    return null;
+  public Invoice createInvoice(Invoice invoice) {
+    return invoice;
   }
 }
