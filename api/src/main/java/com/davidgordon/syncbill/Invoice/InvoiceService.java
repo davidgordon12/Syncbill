94c6e0e339710601f3b6aceca709ae00b44823e3
tehtko <moderndsnsam@gmail.com>
2023-08-29 20:05:53 -0400

    Create InvoiceRepository

--- api/src/main/java/com/davidgordon/syncbill/Invoice/InvoiceService.java
+++ api/src/main/java/com/davidgordon/syncbill/Invoice/InvoiceService.java
@@ -1,5 +1,13 @@
 package com.davidgordon.syncbill.Invoice;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class InvoiceService {
   
+  @Autowired
+  public InvoiceService() {
+
+  }
+
+
 }
